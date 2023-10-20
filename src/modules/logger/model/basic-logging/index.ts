259eab2b1d75e81adf3f58e61c93570d5145bdb3
Karl-Johan Bailey <karl@cenedex.com>
2023-10-20 12:24:02 -0400

    initialize: added basic setup and yarn lock

---
+++ src/modules/logger/model/basic-logging/index.ts
@@ -0,0 +1,23 @@
+import { ILogger } from "..";
+
+export default class MyLogger implements ILogger {
+    error(message: string, data?: any) {
+      console.error(`[${new Date()}] ERROR: ${message}`, data);
+    }
+  
+    debug(message: string, data?: any) {
+        console.debug(`[${new Date()}] DEBUG: ${message}`, data);
+    }
+  
+    info(message: string, data?: any) {
+        console.log(`[${new Date()}] INFO: ${message}`, data);
+    }
+  
+    warning(message: string, data?: any) {
+        console.warn(`[${new Date()}] WARN: ${message}`, data);
+    }
+  
+    fatal(message: string, data?: any) {
+        console.error(`[${new Date()}] FATAL: ${message}`, data);
+    }
+}
